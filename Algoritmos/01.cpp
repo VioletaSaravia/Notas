dbb5b097fcb377b6c89655ff6f9858cca7fe9c4d
Violeta Saravia <saraviavioleta@gmail.com>
2022-10-28 22:23:12 -0300

    segfault fix :)

--- Algoritmos/01.cpp
+++ Algoritmos/01.cpp
@@ -1,39 +1,32 @@
-#include <iostream> // ol' reliable
+#include <iostream> // import std no me anda :/
 #include <vector>
 
-std::vector<int> MergeSort(const std::vector<int> &l)
+std::vector<int> MergeSort(const std::vector<int> &l) // TODO: sort in same object
 {
-	std::vector<int> output;
+	std::vector<int> output; // (size(l)); TODO: fixed size output
 	if (size(l) > 2)
 	{
 		std::vector<int> first_half;
 		std::vector<int> second_half;
-		copy(*l.begin(), l.begin() + int(size(l) / 2), first_half);
-		copy(l.begin() + int(size(l) / 2), l.end(), first_half);
+		std::vector<int>::const_iterator halve = l.begin() + int(size(l) / 2);
+		copy(l.begin(), halve, back_inserter(first_half));
+		copy(halve, l.end(), back_inserter(second_half));
 
 		// parte logaritmica de O()
 		first_half = MergeSort(first_half);
 		second_half = MergeSort(second_half);
 
+		auto first_i = first_half.begin();
+		auto second_i = second_half.begin();
 		for (auto &i : l)
 		{
-			auto first_i = first_half.begin();
-			auto second_i = second_half.begin();
-			// if (first_i == first_half.end())
-			// {
-			// 	copy(second_i, second_half.end(), output); // NO
-			// }
-			// if (second_i == second_half.end())
-			// {
-			// 	copy(first_i, first_half.end(), output); // NO
-			// }
-
+			// TODO: end case
 			if (*first_i < *second_i)
 			{
 				output.push_back(*first_i);
 				++first_i;
 			}
-			else if (*second_i < *first_i) // valores unicos en l
+			else if (*second_i < *first_i) // asume valores unicos en l
 			{
 				output.push_back(*second_i);
 				++second_i;
@@ -48,28 +41,17 @@ std::vector<int> MergeSort(const std::vector<int> &l)
 		}
 		return output;
 	}
-	else if (size(l) == 2)
+	else if ((size(l) == 2) && (l[0] > l[1]))
 	{
-		if (l[0] > l[1])
-		{
-			output[0], output[1] = l[1], l[0];
-		}
-		else if (l[0] < l[1])
-		{
-			copy(l.begin(), l.end(), output);
-		}
-		return output;
+		return std::vector<int>{l[1], l[0]};
 	}
 	return l;
 }
 
 int main()
 {
-	std::vector<int> example_list{5, 4, 1, 8, 7, 2, 6, 3};
-	for (char i : MergeSort(example_list))
-	{
-		std::cout << i << ' ';
-	}
+	auto example_list = {5, 4, 1, 8, 7, 2, 6, 3}; // auto no es vector<int> ???
+	auto result = MergeSort(example_list);
 
 	return 0;
 }
