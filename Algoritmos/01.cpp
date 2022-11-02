9a6e980cdd97e119815b740829c8c51653cae413
Violeta Saravia <saraviavioleta@gmail.com>
2022-11-02 16:48:30 -0300

    Semana 2

--- Algoritmos/01.cpp
+++ Algoritmos/01.cpp
@@ -1,5 +1,9 @@
 #include <iostream> // import std no me anda :/
 #include <vector>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 // usar https://bartoszmilewski.com/2009/10/21/what-does-haskell-have-to-do-with-c/
 // para implementar funcion con distintos templates?¿
@@ -52,40 +56,53 @@ std::vector<int> MergeSort(const std::vector<int> &l) // TODO: sort in same obje
 
 int CountInversions(const std::vector<int> &l)
 {
-	int left_invs;
-	int right_invs;
-	int split_invs;
 	auto l_size = size(l);
-
-	std::vector<int> left_half;
-	std::vector<int> right_half;
-	std::vector<int>::const_iterator halve = l.begin() + int(l_size / 2);
-	copy(l.begin(), halve, back_inserter(left_half));
-	copy(halve, l.end(), back_inserter(right_half));
-
 	if (l_size > 2)
 	{
-		left_invs = CountInversions(left_half);
-		right_invs = CountInversions(right_half);
+		std::vector<int> left_half;
+		std::vector<int> right_half;
+		auto halve = l.begin() + (l_size / 2);
+		copy(l.begin(), halve, back_inserter(left_half));
+		copy(halve, l.end(), back_inserter(right_half));
+
+		int left_invs = CountInversions(left_half);
+		int right_invs = CountInversions(right_half);
+		left_half = MergeSort(left_half);
+		right_half = MergeSort(right_half);
+		int split_invs = 0;
 
 		auto left_i = left_half.begin();
 		auto right_i = right_half.begin();
-		for (auto &i : l)
+		while (left_i != left_half.end())
 		{
+			// if (right_i == right_half.end())
+			// {
+			// 	++left_i
+			// }
 			if (*left_i <= *right_i)
 			{
 				++left_i;
+				right_i = right_half.begin();
 			}
 			else if (*right_i < *left_i)
 			{
-				++split_invs; // no cuenta todo
-				++right_i;
+				++split_invs;
+				// split_invs += right_half.end() - right_i;
+
+				if (right_i == right_half.end() - 1 && left_i == left_half.end() - 1)
+				{
+					break;
+				}
+				if (right_i != right_half.end() - 1)
+				{
+					++right_i;
+				}
 			}
-			else if (*right_i == *left_i)
-			{
-				++left_i;
-				++right_i;
-			}
+			// else if (*right_i == *left_i) // no hay = en el ejercicio
+			// {
+			// 	++left_i;
+			// 	++right_i;
+			// }
 		}
 
 		return left_invs + right_invs + split_invs;
@@ -152,6 +169,54 @@ std::vector<point> ClosestPair(const std::vector<point> &p)
 	return closest;
 }
 
+// std::vector<int> operator*(
+// 	const std::vector<int> &op1,
+// 	const std::vector<int> &op2)
+// {
+// 	/* asume que op1 y op2 tienen el mismo largo*/
+// 	/* T(1) <= constante */
+// 	if (size(op1) == 1)
+// 	{
+// 		return {op1[0] * op2[0]};
+// 	}
+
+// 	std::vector<int> a, b, c, d;
+
+// 	auto op1_halve = op1.begin() + int(size(op1) / 2);
+// 	auto op2_halve = op2.begin() + int(size(op2) / 2);
+
+// 	copy(op1.begin(), op1_halve, back_inserter(a));
+// 	copy(op1_halve, op1.end(), back_inserter(b));
+// 	copy(op2.begin(), op2_halve, back_inserter(c));
+// 	copy(op2_halve, op2.end(), back_inserter(d));
+
+// 	auto p1 = a * c;
+// 	auto p2 = b * d;
+// 	auto p3 = (a + b) * (c + d);
+// 	auto p4 = p3 + -1 * p1 + -1 * p2;
+
+// 	return {pow(10, size(op1)) * p1 + pow(10, size(op1) / 2) * p4 + p2};
+// }
+
+// std::vector<int> operator*(
+// 	const auto &n,
+// 	const std::vector<int> &v)
+// {
+// 	std::vector<int> result;
+// 	for (auto &i : v)
+// 	{
+// 		result.push_back(i * n);
+// 	}
+// 	return result;
+// }
+
+// std::vector<int> operator+(
+// 	const std::vector<int> &v1,
+// 	const std::vector<int> &v2)
+// {
+// 	/* :$ */
+// }
+
 int main()
 {
 	auto example_list = {5, 4, 1, 8, 7, 2, 6, 3}; // auto no es vector<int> ???
@@ -160,7 +225,27 @@ int main()
 
 	auto result = MergeSort(example_list);
 	auto result2 = CountInversions(example_list);
-	auto result3 = matrixA * matrixB;
+	auto list2 = {2, 1};
+	auto test1 = CountInversions({2, 1, 3});
+	// auto result3 = matrixA * matrixB;
+
+	std::vector<int> example_intarray;
+	std::ifstream infile("IntArray.txt");
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+		int a;
+		if (!(iss >> a))
+		{
+			break;
+		}
+
+		example_intarray.push_back(a);
+	}
+
+	// TODO: ARREGLAR MERGESORT FINAL Y ESTAMOS
+	auto result4 = CountInversions(example_intarray);
 
 	return 0;
 }
