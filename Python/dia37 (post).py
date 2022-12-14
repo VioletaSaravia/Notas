982008d05cc5f9b95e11e646775e801ebdfac3dc
Violeta Saravia <saraviavioleta@gmail.com>
2022-12-14 16:46:51 -0300

    .

--- Python/dia37 (post).py
+++
@@ -1,7 +0,0 @@
-from datetime import date, datetime
-
-test = str(datetime.now()).split(" ")[0].replace("-", "")
-
-print(test)
-
-
--- Python/notes.py
+++
@@ -1,3 +0,0 @@
-with open("base.txt", "w") as base:
-	for note in range(128):
-		base.write(f'{note}, 0;\n')
